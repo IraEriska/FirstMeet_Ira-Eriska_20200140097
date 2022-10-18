8f5e544fe5ee82f99ac2d58ca3b5eda2b0e87445
IraEriska <ira09155@gmail.com>
2022-10-18 21:20:42 +0700

    first projects

---
+++ src/main/java/pabd/learningmigrateDB/LearningmigrateDbApplication.java
@@ -0,0 +1,13 @@
+package pabd.learningmigrateDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningmigrateDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningmigrateDbApplication.class, args);
+	}
+
+}
